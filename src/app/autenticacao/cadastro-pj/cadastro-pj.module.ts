14916692683d90a983203222a718d6b737077886
Ricardo Pesce <rspesce@gmail.com>
2021-01-21 03:25:06 -0300

    criando formulário de cadastro de PJ

--- src/app/autenticacao/cadastro-pj/cadastro-pj.module.ts
+++ src/app/autenticacao/cadastro-pj/cadastro-pj.module.ts
@@ -2,6 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { CadastrarPjComponent, CadastroPjComponent } from './components';
 import { CadastroPjRoutes } from './cadastro-pj-routing.module';
@@ -16,7 +25,15 @@ import { CadastroPjRoutes } from './cadastro-pj-routing.module';
   imports: [
     CommonModule,
     RouterModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatInputModule,
+    MatButtonModule,
+    MatListModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatSnackBarModule
   ]
 })
 export class CadastroPjModule { }
--- src/app/autenticacao/cadastro-pj/components/cadastro-pj.component.ts
+++ src/app/autenticacao/cadastro-pj/components/cadastro-pj.component.ts
@@ -3,7 +3,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 
 @Component({
     template: `
-    <h2 FlexLayoutAlign="center">Cadastro de Pessoa Jurídica</h2>
+    <h2 fxLayoutAlign="center">Cadastro de Pessoa Jurídica</h2>
     <router-outlet></router-outlet>
     `
 })
