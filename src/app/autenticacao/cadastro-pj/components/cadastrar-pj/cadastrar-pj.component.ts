fbcdb3308e08b13f0eb9157fc3be50d006ec9558
Ricardo Pesce <rspesce@gmail.com>
2021-01-22 22:36:28 -0300

    criando o validador de CNPJ

--- src/app/autenticacao/cadastro-pj/components/cadastrar-pj/cadastrar-pj.component.ts
+++ src/app/autenticacao/cadastro-pj/components/cadastrar-pj/cadastrar-pj.component.ts
@@ -4,7 +4,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 
 import { CadastroPJ } from '../../models';
-import { CpfValidator } from '../../../../shared/validators';
+import { CpfValidator, CnpjValidator } from '../../../../shared/validators';
 
 @Component({
   selector: 'app-cadastrar-pj',
@@ -31,7 +31,7 @@ export class CadastrarPjComponent implements OnInit {
       senha: ['', [Validators.required, Validators.minLength(6)]],
       cpf: ['', [Validators.required, CpfValidator]],
       razaoSocial: ['', [Validators.required, Validators.minLength(5)]],
-      cnpj: ['', [Validators.required]]
+      cnpj: ['', [Validators.required, CnpjValidator]]
     });
   }
 
