0d224d16bf0a3fe921337a8a55ff6a98f3ccf283
Ricardo Pesce <rspesce@gmail.com>
2021-01-23 04:25:50 -0300

    implementando o componente de lançamento

--- src/app/funcionario/components/lancamento/lancamento.component.ts
+++ src/app/funcionario/components/lancamento/lancamento.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+
+import {
+  Tipo
+} from '../../../shared';
+
+import * as moment from 'moment';
+
+declare var navigator: any;
 
 @Component({
   selector: 'app-lancamento',
@@ -7,9 +17,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LancamentoComponent implements OnInit {
 
-  constructor() { }
+  private dataAtualEn: string;
+  dataAtual: string;
+  geoLocation: string;
+  ultimoTipoLancado: string;
+
+  constructor(
+    private snackBar: MatSnackBar,
+    private router: Router ) { }
 
   ngOnInit(): void {
+    this.dataAtual = moment().format('DD/MM/YYYY HH:mm:ss');
+    this.dataAtualEn = moment().format('YYYY-MM-DD HH:mm:ss');
+    this.obterGeoLocation();
+    this.ultimoTipoLancado = '';
+    this.obterUltimoLancamento();
+  }
+
+  obterGeoLocation(): string {
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position =>
+        this.geoLocation = `${position.coords.latitude}, ${position.coords.longitude}`);
+    }
+    return  '';
+  }
+
+  iniciarTrabalho() {
+    this.cadastrar(Tipo.INICIO_TRABALHO);
+  }
+
+  terminarTrabalho() {
+    this.cadastrar(Tipo.TERMINO_TRABALHO);
+  }
+
+  iniciarAlmoco() {
+    this.cadastrar(Tipo.INICIO_ALMOCO);
+  }
+
+  terminarAlmoco() {
+    this.cadastrar(Tipo.TERMINO_ALMOCO);
+  }
+
+  obterUltimoLancamento() {
+    this.ultimoTipoLancado = '';
+  }
+
+  cadastrar(tipo: Tipo) {
+    alert(`Tipo: ${tipo}, dataAtualEn: ${this.dataAtualEn},
+      geolocation: ${this.geoLocation}`);
+  }
+
+  obterUrlMapa(): string {
+    return "http://www.google.com/maps/search/?api=1&query=" +
+      this.geoLocation;
+  }
+
+  exibirInicioTrabalho(): boolean {
+    return this.ultimoTipoLancado == '' || 
+        this.ultimoTipoLancado == Tipo.TERMINO_TRABALHO;
+  }
+
+  exibirTerminoTrabalho(): boolean {
+    return this.ultimoTipoLancado == Tipo.INICIO_TRABALHO ||
+        this.ultimoTipoLancado == Tipo.TERMINO_ALMOCO;
+  }
+
+  exibirInicioAlmoco(): boolean {
+    return this.ultimoTipoLancado == Tipo.INICIO_TRABALHO;
+  }
+
+  exibirTerminoAlmoco(): boolean {
+    return this.ultimoTipoLancado == Tipo.INICIO_ALMOCO;
   }
 
 }
--- src/app/funcionario/funcionario.module.ts
+++ src/app/funcionario/funcionario.module.ts
@@ -2,6 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatCardModule } from '@angular/material/card';
 
 import { 
   ListagemComponent, 
@@ -19,7 +31,19 @@ import {
   imports: [
     CommonModule,
     RouterModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    ReactiveFormsModule,
+
+    MatInputModule,
+    MatButtonModule,
+    MatListModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatSnackBarModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatCardModule
   ]
 })
 export class FuncionarioModule { }
