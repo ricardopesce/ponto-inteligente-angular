5679bb7fcdf157b2e2fa1b130af93bc14d0e34e1
Ricardo Pesce <rspesce@gmail.com>
2021-01-27 02:25:57 -0300

    criando a listagem de lancamento (lazy loading datatable) em admin/

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatPaginatorIntl } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+
 import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { SharedModule } from '../shared/shared.module';
+
+import {
+  HttpUtilService,
+  LancamentoService,
+  PtBrMatPaginatorIntl
+} from '../shared';
 
 import {
   ListagemComponent,
@@ -13,16 +40,42 @@ import {
 
 
 @NgModule({
+  imports: [
+    CommonModule,
+    RouterModule,
+    FlexLayoutModule,
+    ReactiveFormsModule,
+    FormsModule,
+
+    MatInputModule,
+    MatButtonModule,
+    MatListModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatSnackBarModule,
+    MatTableModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatDialogModule,
+    MatPaginatorModule,
+    MatSortModule,
+    SharedModule
+  ],
   declarations: [
     ListagemComponent, 
     CadastroComponent, 
     AtualizacaoComponent,
     AdminComponent
   ],
-  imports: [
-    CommonModule,
-    RouterModule,
-    FlexLayoutModule
-  ]
+  
+  providers: [
+    LancamentoService,
+    HttpUtilService,
+    MatPaginatorIntl,
+    { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' },
+    { provide: MatPaginatorIntl, useClass: PtBrMatPaginatorIntl }
+  ],
 })
 export class AdminModule { }
--- src/app/admin/components/listagem/listagem.component.ts
+++ src/app/admin/components/listagem/listagem.component.ts
@@ -1,4 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { MatSelect } from '@angular/material/select';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { PageEvent } from '@angular/material/paginator';
+import { Sort } from '@angular/material/sort';
+
+import {
+  LancamentoService,
+  Lancamento,
+  Funcionario,
+  Tipo,
+  HttpUtilService
+} from '../../../shared';
+
 
 @Component({
   selector: 'app-listagem',
@@ -7,9 +25,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListagemComponent implements OnInit {
 
-  constructor() { }
+  dataSource: MatTableDataSource<Lancamento>;
+  colunas: string[] = ['data', 'tipo', 'localizacao', 'acao'];
+  funcionarioId: string;
+  totalLancamentos: number;
+
+  private pagina: number;
+  private ordem: string;
+  private direcao: string;
+
+  constructor(
+    private lancamentoService: LancamentoService,
+    private httpUtil: HttpUtilService,
+    private snackBar: MatSnackBar,
+    private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.pagina = 0;
+    this.ordemPadrao();
+    this.exibirLancamentos();
+  }
+
+  ordemPadrao() {
+    this.ordem = 'data';
+    this.direcao = 'DESC';
+  }
+
+  exibirLancamentos() {
+    this.funcionarioId = '3';
+
+    this.lancamentoService.listarLancamentosPorFuncionario(
+      this.funcionarioId, this.pagina, this.ordem, this.direcao)
+      .subscribe(
+        data => {
+          this.totalLancamentos = data['data'].totalElements;
+          const lancamentos = data['data'].content as Lancamento[];
+          this.dataSource = new MatTableDataSource<Lancamento>(lancamentos);
+        },
+        err => {
+          const msg: string = "Erro obtendo lançamentos.";
+          this.snackBar.open(msg, "Erro", { duration: 5000 });
+        }
+      );
+  }
+
+  remover(lancamentoId: string) {
+    alert(lancamentoId);
+  }
+
+  paginar(pageEvent: PageEvent) {
+    this.pagina = pageEvent.pageIndex;
+    this.exibirLancamentos();
+  }
+
+  ordenar(sort: Sort){
+    if(sort.direction == '') 
+      this.ordemPadrao();
+    else {
+      this.ordem = sort.active;
+      this.direcao = sort.direction.toUpperCase();
+    }
+    this.exibirLancamentos();    
   }
 
 }
